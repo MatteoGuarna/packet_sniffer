1c3f95bd1aa949ac709031afaafd3b131b9f25eb
AleCir99 <s306000@studenti.polito.it>
2022-08-11 18:39:27 +0200

    ipv6

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use sprintf::sprintf;
 
 fn main() {
     let devs = Device::list().unwrap();
-    let d = devs.get(1).unwrap();
+    let d = devs.get(2).unwrap();
     let mut cap = d.clone().open().unwrap();
 
     println!(
-        "{0: <12} | {1: <15} | {2: <15} | {3: <18} | {4: <15} | {5: <11}",
+        "{0: <12} | {1: <18} | {2: <18} | {3: <18} | {4: <15} | {5: <11}",
         "IP Protocol", "Destination IP", "Source IP", "Transport Protocol", "Destination Port", "Source Port"
     );
     /*
@@ -26,22 +26,16 @@ fn main() {
                 IpHeader::Version4(h, _e) => {
                     let dest = sprintf!("%d.%d.%d.%d", h.destination[0],h.destination[1],h.destination[2],h.destination[3]);
                     let sour = sprintf!("%d.%d.%d.%d", h.source[0],h.source[1],h.source[2],h.source[3]);
-                    print!("{0: <12} | {1:<15} | {2:<15} |", "IPv4", dest.unwrap(), sour.unwrap());
+                    print!("{0: <12} | {1:<18} | {2:<18} |", "IPv4", dest.unwrap(), sour.unwrap());
                 }
                 IpHeader::Version6(h, _e) => {
-                    print!("  {0: <10} |", "IPv6");
-                    println!("-- Destination IP: {:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}", 
+                    let dest = sprintf!("%x:%x:%x:%x:%x:%x:%x:%x", 
                         h.destination[0],h.destination[1],h.destination[2],h.destination[3],
-                        h.destination[4],h.destination[5],h.destination[6],h.destination[7],
-                        h.destination[8],h.destination[9],h.destination[10],h.destination[11],
-                        h.destination[12],h.destination[13],h.destination[14],h.destination[15]
-                    );
-                    println!("-- Source IP: {:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}:{:#01x}{:#01x}", 
+                        h.destination[4],h.destination[5],h.destination[6],h.destination[7]);
+                    let sour = sprintf!("%x:%x:%x:%x:%x:%x:%x:%x",
                         h.source[0],h.source[1],h.source[2],h.source[3],
-                        h.source[4],h.source[5],h.source[6],h.source[7],
-                        h.source[8],h.source[9],h.source[10],h.source[11],
-                        h.source[12],h.source[13],h.source[14],h.source[15] 
-                    );
+                        h.source[4],h.source[5],h.source[6],h.source[7]);
+                    print!("{0: <12} | {1:<18} | {2:<18} |", "IPv6", dest.unwrap(), sour.unwrap());
                 }
             }
             match value.transport.unwrap() {
